9fb11b6feec0260872d77ddf084c954150a55a36
Peanutt42 <97802125+Peanutt42@users.noreply.github.com>
2022-12-16 17:47:55 +0100

    Added DEBUG and ECHO_ENABLED

--- LinienSensor.h
+++ LinienSensor.h
@@ -3,28 +3,26 @@
 
 struct LinienSensorResult {
   int Left, Right, Center;
+  LinienSensorResult(int left = 0, int right = 0, int center = 0)
+    : Left(left), Right(right), Center(center) {}
 };
 
 class LinienSensor {
-  public:
-    LinienSensor() {}
+public:
+  LinienSensor() {}
 
-    void Init(int pinA1, int pinA2, int pinA3) {
-      m_PinA1 = pinA1;
-      m_PinA2 = pinA2;
-      m_PinA3 = pinA3;
-    }
+  void Init(int pinA1, int pinA2, int pinA3) {
+    m_PinA1 = pinA1;
+    m_PinA2 = pinA2;
+    m_PinA3 = pinA3;
+  }
 
-    LinienSensorResult Messure() {
-      LinienSensorResult result;
-      result.Left = analogRead(m_PinA1);
-      result.Center = analogRead(m_PinA2);
-      result.Right = analogRead(m_PinA3);
-      return result;
-    }
+  LinienSensorResult Messure() {
+    return LinienSensorResult{ analogRead(m_PinA1), analogRead(m_PinA3), analogRead(m_PinA2) };
+  }
 
-  private:
-    int m_PinA1, m_PinA2, m_PinA3;
+private:
+  int m_PinA1, m_PinA2, m_PinA3;
 };
 
-#endif // LINIENSENSOR_H
+#endif  // LINIENSENSOR_H
--- Petronics-Hugo.ino
+++ Petronics-Hugo.ino
@@ -3,34 +3,55 @@
 #include "Motor.h"
 
 #define MOTOR_STEER_SPEED 0
-#define MOTOR_DRIVE_SPEED 100
+#define MOTOR_DRIVE_SPEED_L 85
+#define MOTOR_DRIVE_SPEED_R 92
 
+#define DEBUG false
+#define ECHO_ENABLED false
+
+#if ECHO_ENABLED
 EchoSensor DistanceSensor;
+#endif
 LinienSensor SteeringSensor;
 Motor LeftMotor, RightMotor;
 
-bool steerLeft, steerRight;
-bool drive;
+bool steerLeft = false, steerRight = false;
+bool drive = true;
+LinienSensorResult lenkungResult;
+
+#define IS_LINE_THRESHOLD 150
 
 void setup() {
+#if DEBUG
   Serial.begin(9600);
+#endif
 
+#if ECHO_ENABLED
   DistanceSensor.Init(3, 4);
+#endif
   SteeringSensor.Init(A1, A2, A3);
 
   LeftMotor.Init(10, 9, 8, true);
   RightMotor.Init(5, 6, 7, false);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-
 void loop() {
+  drive = true;
+
+#if ECHO_ENABLED
   // Print out sensor raw values for debugging
   long frontDistance = DistanceSensor.GetDistance(Metric::Centimeter);
-  bool obsticalInTheWay = frontDistance < 5; // if front distance is less than 5cm away
+  bool obsticalInTheWay = frontDistance < 5;  // if front distance is less than 5cm away
+#endif
+#if DEBUG && ECHO_ENABLED
   Serial.print("FrontDistance: ");
   Serial.print(frontDistance);
+#endif
 
-  auto lenkungResult = SteeringSensor.Messure();
+  lenkungResult = SteeringSensor.Messure();
+#if DEBUG
   Serial.print("    Raw Sensor: [L: ");
   Serial.print(lenkungResult.Left);
   Serial.print(", C: ");
@@ -38,50 +59,59 @@ void loop() {
   Serial.print(", R: ");
   Serial.print(lenkungResult.Right);
   Serial.print(']');
+#endif
 
   // Configure driving direction based on sensor values
-  if (IsLine(lenkungResult.Center)) {
+  bool lineIsLeft = lenkungResult.Left > IS_LINE_THRESHOLD,
+       lineIsRight = lenkungResult.Right > IS_LINE_THRESHOLD,
+       lineIsCenter = lenkungResult.Center > IS_LINE_THRESHOLD;
+  if (lineIsCenter) {
     steerLeft = false;
     steerRight = false;
-  }
-  else if (IsLine(lenkungResult.Left)) {
+  } else if (lineIsLeft) {
     steerLeft = true;
     steerRight = false;
-  }
-  else if (IsLine(lenkungResult.Right)) {
+  } else if (lineIsRight) {
     steerRight = true;
     steerLeft = false;
   }
-  if (IsLine(lenkungResult.Center) && IsLine(lenkungResult.Left) && IsLine(lenkungResult.Right)) {
+  if (lineIsLeft && lineIsRight && lineIsCenter) {
     steerLeft = steerRight = false;
+    drive = false;
   }
 
-  drive = !obsticalInTheWay;
+#if ECHO_ENABLED
+  if (obsticalInTheWay) drive = false;
+#endif
 
   // Apply configured driving directions to motors
   if (!drive) {
+#if DEBUG
+    Serial.println(" standing still");
+#endif
     LeftMotor.SetSpeed(0);
     RightMotor.SetSpeed(0);
-  }
-  else if (!steerLeft && !steerRight) {
-    if (drive) Serial.println(" driving forward");
-    else Serial.println(" standing still");
-
-    LeftMotor.SetSpeed(MOTOR_DRIVE_SPEED);
-    RightMotor.SetSpeed(MOTOR_DRIVE_SPEED);
-  }
-  else if (steerLeft) {
+  } else if (!steerLeft && !steerRight) {
+#if DEBUG
+    Serial.println(" driving forward");
+#endif
+    LeftMotor.SetSpeed(MOTOR_DRIVE_SPEED_L);
+    RightMotor.SetSpeed(MOTOR_DRIVE_SPEED_R);
+  } else if (steerLeft) {
+#if DEBUG
     Serial.println(" driving left");
-    LeftMotor.SetSpeed(MOTOR_DRIVE_SPEED);
+#endif
+    LeftMotor.SetSpeed(MOTOR_DRIVE_SPEED_L);
     RightMotor.SetSpeed(MOTOR_STEER_SPEED);
-  }
-  else if (steerRight) {
+  } else if (steerRight) {
+#if DEBUG
     Serial.println(" driving right");
+#endif
     LeftMotor.SetSpeed(MOTOR_STEER_SPEED);
-    RightMotor.SetSpeed(MOTOR_DRIVE_SPEED);
+    RightMotor.SetSpeed(MOTOR_DRIVE_SPEED_R);
   }
-}
 
-inline bool IsLine(int messuredValue) {
-  return messuredValue > 150; // needs to be tested and adjusted for every situation!
+  static bool high = true;
+  high = !high;
+  digitalWrite(LED_BUILTIN, high);
 }
